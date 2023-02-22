e8716de4f2437a53729d62339b713a9f088e9edf
Tatiespi <tatiana.espinosa@uao.edu.co>
2023-02-22 14:32:05 -0500

    challenge 3

---
+++ src/challenge3.js
@@ -0,0 +1,28 @@
+const App = () => {
+    const [counter, setCounter] = useState(0);
+  
+    const handleSubtract = () => {
+     if(counter>0){
+        setCounter(counter - 1);
+      }
+      
+    };
+    const addubtract = () => {
+      setCounter(counter + 1);
+    };
+  
+    const handleReset = () => {
+     setCounter(0);
+    };
+  
+    return (
+      <div>
+       <h1>Counter: {counter}</h1>
+       <button onClick={handleSubtract}>Subtract</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={addubtract}>add</button>    
+       </div>
+   );
+  };
+  
+  export default App;
