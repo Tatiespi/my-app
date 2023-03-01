61163017201500db348fe9ce02a38d3d607d7903
Tatiespi <tatiana.espinosa@uao.edu.co>
2023-03-01 18:03:24 -0500

    challenge 4

--- src/App.js
+++ src/App.js
@@ -3,49 +3,6 @@ import "./index.js"
 
 
 
-
-//Challenge 4
-//EMPIEZAAAA
-
-
-//TERMINAAAA
-import React, { useState, useEffect } from 'react';
-
-const GifGrid = () => {
-  const [gifs, setGifs] = useState([]);
-
-  const getGifs = async () => {
-    const response = await fetch('https://api.giphy.com/v1/gifs/search?api_key=eEufhiRhqRyc5888Bj80CEtTnWnI18ba&q=$%7Bcategory%7D&limit=15&offset=0&rating=g&lang=en');
-    const data = await response.json();
-    const gifs = data.data;
-    setGifs(gifs);
-    console.log(gifs);
-  }
-
-  useEffect(() => {
-    getGifs();
-  }, []);
-
-  return (
-    <div className="gif-grid">
-      {gifs.map(gif => (
-        <GifItem key={gif.id} gif={gif} />
-      ))}
-    </div>
-  );
-}
-
-const GifItem = ({ gif }) => {
-  return (
-    <div className="gif-item">
-      <h3>{gif.title}</h3>
-    </div>
-  );
-}
-
-export default GifGrid; 
-/*
-
 import React, { useState } from 'react';
 
 function App() {
@@ -79,7 +36,6 @@ function App() {
 }
 
 export default App;
-*/
-//hola
+
 
 
