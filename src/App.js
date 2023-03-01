27929138284511eadc7b376a510e67170a542c32
Tatiespi <tatiana.espinosa@uao.edu.co>
2023-03-01 18:04:00 -0500

    challenge 4

--- src/App.js
+++ src/App.js
@@ -1,43 +1,7 @@
 import "./App.css";
 import "./index.js"
 
-import { useState } from 'react';
 
-const App =() =>{
-
- const [counter, setCounter] = useState(0);
-
-  const handleSubtract = () => {
-   if(counter>0){
-      setCounter(counter - 1);
-    }
-    
-  };
-  const addubtract = () => {
-    setCounter(counter + 1);
-  };
-
-  const handleReset = () => {
-   setCounter(0);
-  };
-
-  return (
-    <div>
-     <h1>Counter: {counter}</h1>
-     <button onClick={handleSubtract}>Subtract</button>
-      <button onClick={handleReset}>Reset</button>
-      <button onClick={addubtract}>add</button>    
-     </div>
- );
-
-  };
-
-
-
-export default App;
-
-
-/*
 import React, { useState, useEffect } from 'react';
 
 const GifGrid = () => {
@@ -73,7 +37,6 @@ const GifItem = ({ gif }) => {
 }
 
 export default GifGrid; 
-*/
 
 
 
